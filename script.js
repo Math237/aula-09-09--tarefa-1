6934b006a67cb6c5d30827ee058186aad93ee9f9
Math237 <richardgarcia28688@gmail.com>
2024-09-09 22:26:31 -0300

    aula 0909ex1

---
+++ script.js
@@ -0,0 +1,18 @@
+/*
+O cardápio de uma lanchonete é o seguinte:
+Especificação Preço unitário 
+100 Cachorro quente 1,10 
+101 Bauru simples 1,30 
+102 Bauru c/ovo 1,50 
+103 Hamburger 1,10 
+104 Cheeseburger 1,30 
+
+Escrever um algoritmo que leia o código do lanche pedido, a quantidade e calcule o valor a ser pago por aquele lanche. 
+*/
+console.log("Bem vindo ao nosso restaurante! nós temos os seguintes pratos:");
+console.log("100 Cachorro quente 1,10");
+console.log("101 Bauru simples 1,30 ");
+console.log("102 Bauru c/ovo 1,50 ");
+console.log("103 Hamburger 1,10 ");
+console.log("104 Cheeseburger 1,30 ");
+prompt("O cardápio de uma lanchonete é o seguinte:                                         Especificação Preço unitário 100 Cachorro quente 1,10 101 Bauru simples 1,30 102 Bauru c/ovo 1,50 103 Hamburger 1,10 104 Cheeseburger 1,30 ");
